a7edc061141f769aca9788c2c4280c87c6c619ea
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-18 03:51:38 +0500

    completed sqlserver

---
+++ Student.API/Controllers/StudentsController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using User.API.Data;
+using User.API.Models;
+
+namespace User.API.Controllers;
+
+[Route("api/students")]
+[ApiController]
+public class StudentsController : ControllerBase
+{
+    private readonly StudentDbContext _dbContext;
+
+    public StudentsController(StudentDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var users = await _dbContext.Students.ToListAsync();
+        return Ok(users);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(StudentDto userDto)
+    {
+        var userModel = new StudentModel
+        {
+            FirstName = userDto.FirstName,
+            LastName = userDto.LastName,
+            Email = userDto.Email,
+            Password = userDto.Password
+        };
+
+        _dbContext.Students.Add(userModel);
+        await _dbContext.SaveChangesAsync();
+
+        return Ok(userModel);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(int id, StudentDto userDto)
+    {
+        var userModel = await _dbContext.Students.FindAsync(id);
+
+        if (userModel == null)
+        {
+            return NotFound();
+        }
+
+        userModel.FirstName = userDto.FirstName;
+        userModel.LastName = userDto.LastName;
+        userModel.Email = userDto.Email;
+        userModel.Password = userDto.Password;
+
+        _dbContext.Entry(userModel).State = EntityState.Modified;
+        await _dbContext.SaveChangesAsync();
+
+        return Ok(userModel);
+    }
+
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var user = await _dbContext.Students.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _dbContext.Students.Remove(user);
+        await _dbContext.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById(int id)
+    {
+        var user = await _dbContext.Students.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+}
